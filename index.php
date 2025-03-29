8dd2cf5e0d391ba601743545b35dc5c31bc5f3bf
Ali Raza <alirazakhalid1129@gmail.com>
2025-03-29 21:55:05 +0500

    update styles.css

--- index.php
+++ index.php
@@ -3,45 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php First website</title>
+    <title>PHP First Website</title>
     <link rel="stylesheet" href="./style/styles.css">
 </head>
 <body>
-    <main style="background: inherit;">
-        <h1>CREATE Account</h1>
-        <form action="includes/formhandler.inc.php" method="post">
-            <input required type="text" name="username" placeholder="username">
-            <input required type="password" name="password" placeholder="password">
-            <button>SignUp</button>
-        </form>
-    </main>
 
-    <main style="background: violet;">
-        <h1>UPDATE Account Details</h1>
+    <section class="account-section create">
+        <h1>Create Account</h1>
+        <form action="includes/formhandler.inc.php" method="post">
+            <input required type="text" name="username" placeholder="Username">
+            <input required type="password" name="password" placeholder="Password">
+            <button>Sign Up</button>
+        </form>
+    </section>
+
+    <section class="account-section update">
+        <h1>Update Account Details</h1>
         <form action="includes/updateaccount.inc.php" method="post">
-            <input required type="text" name="username" placeholder="OLD username">
-            <input required type="password" name="password" placeholder="password">
-            <input required type="text" name="newusername" placeholder="NEW username">
+            <input required type="text" name="username" placeholder="Old Username">
+            <input required type="password" name="password" placeholder="Password">
+            <input required type="text" name="newusername" placeholder="New Username">
             <button>Update</button>
         </form>
-    </main>
+    </section>
 
-    <main style="background: red;">
-        <h1>DELETE Account</h1>
+    <section class="account-section delete">
+        <h1>Delete Account</h1>
         <form action="includes/deleteaccount.inc.php" method="post">
-            <input required type="text" name="username" placeholder="username">
-            <input required type="password" name="password" placeholder="password">
+            <input required type="text" name="username" placeholder="Username">
+            <input required type="password" name="password" placeholder="Password">
             <button>Delete</button>
         </form>
-    </main>
+    </section>
 
-    <main class="search">
-        <h1>READ comments</h1>
+    <section class="account-section search">
+        <h1>Read Comments</h1>
         <form action="search.php" method="post">
-            <input type="text" name="search" placeholder="Enter id to search">
+            <input required type="text" name="search" placeholder="Enter username to search">
             <button>Search</button>
         </form>
-    </main>
+    </section>
 
 </body>
 </html>
