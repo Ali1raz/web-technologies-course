7184f59a5fb10caa4509777ed1ce59166dd254ea
Ali Raza <alirazakhalid1129@gmail.com>
2025-04-19 20:38:59 +0500

    reponsive header

--- GrabNGo/script.js
+++ GrabNGo/script.js
@@ -1,1 +1,9 @@
-console.log("ok");
+[
+  document.querySelector("input[type='checkbox'].menu"),
+  document.querySelector("input[type='checkbox'].menu-close"),
+  document.querySelector("ul.menu-list"),
+].forEach((item) =>
+  item.addEventListener("click", () => {
+    document.querySelector("ul").classList.toggle("checked");
+  })
+);
