f80cb6c0d7fca7b50f0cc96c8019d36498eee2aa
Ali Raza <alirazakhalid1129@gmail.com>
2025-05-23 09:36:27 +0500

    add role to user and create product migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,11 +16,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::statement("
+            INSERT INTO users (name, email, password, created_at, updated_at) 
+            VALUES (
+                'Admin User',
+                'admin@example.com',
+                '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                NOW(),
+                NOW()
+            )
+        ");
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::statement("
+            INSERT INTO users (name, email, password, created_at, updated_at) 
+            VALUES (
+                'Customer User',
+                'customer@example.com',
+                '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                NOW(),
+                NOW()
+            )
+        ");
     }
 }
